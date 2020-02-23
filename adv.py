6b0e939ec67f1a1dea989475af563d2ba510664b
Yasirah Krueng <yasirah.krueng@arundo.com>
2020-02-23 13:01:50 -0600

    solve path traversal in less than 2000 moves

--- adv.py
+++ adv.py
@@ -1,9 +1,10 @@
+from ast import literal_eval
 from room import Room
 from player import Player
 from world import World
 
 import random
-from ast import literal_eval
+random.seed()
 
 # Load world
 world = World()
@@ -17,7 +18,7 @@ world = World()
 map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
-room_graph=literal_eval(open(map_file, "r").read())
+room_graph = literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
 
 # Print an ASCII map
@@ -25,11 +26,63 @@ world.print_rooms()
 
 player = Player(world.starting_room)
 
+
+# Utils
+reverse_dir = {'n': 's', 's': 'n', 'w': 'e', 'e': 'w'}
+
+
+def on_move(traversal_graph, from_room, to_room, dir=None):
+    if to_room.id not in traversal_graph:
+        new_room = {'n': None, 's': None, 'w': None, 'e': None}
+        exits = to_room.get_exits()
+
+        for exit in exits:
+            new_room[exit] = '?'
+
+        traversal_graph[to_room.id] = new_room
+    if from_room is not None:
+        traversal_graph[from_room.id][dir] = to_room.id
+        traversal_graph[to_room.id][reverse_dir[dir]] = from_room.id
+
+
+def next_moves(traversal_graph, current_room):
+    seen = set([current_room.id])
+    exits = [{'direction': exit, 'path': []}
+             for exit in traversal_graph[current_room.id].items() if exit[1] is not None and exit[1] not in seen]
+    random.shuffle(exits)
+    while len(exits) > 0:
+        current = exits.pop(0)
+        if current['direction'][1] == '?':
+            current['path'].append(current['direction'][0])
+            return current['path']
+        else:
+            seen.add(current['direction'][1])
+            next_exits = [{
+                'direction': exit,
+                'path': current['path'] + [current['direction'][0]]
+            }
+                for exit in traversal_graph[current['direction'][1]].items()
+                if exit[1] is not None and exit[1] not in seen
+            ]
+            random.shuffle(next_exits)
+            exits = exits + next_exits
+
+
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
+traversal_graph = {}
+on_move(traversal_graph, None, player.current_room)
 
-
+while len(traversal_graph.keys()) != len(world.rooms):
+    room = player.current_room
+    directions = next_moves(traversal_graph, room)
+    for direction in directions:
+        traversal_path.append(direction)
+        current_room = player.current_room
+        player.travel(direction)
+        next_room = player.current_room
+        on_move(traversal_graph, current_room, next_room, direction)
 
 # TRAVERSAL TEST
 visited_rooms = set()
@@ -41,22 +94,22 @@ for move in traversal_path:
     visited_rooms.add(player.current_room)
 
 if len(visited_rooms) == len(room_graph):
-    print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
+    print(
+        f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
 
 
-
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
